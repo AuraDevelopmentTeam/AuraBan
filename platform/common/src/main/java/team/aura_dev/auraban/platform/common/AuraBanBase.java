a92f126575d931062308e0915849c69094b7a79e
BrainStone <yannick@tedworld.de>
2019-08-12 23:52:40 +0200

    Using SLF4J everywhere

--- platform/common/src/main/java/team/aura_dev/auraban/platform/common/AuraBanBase.java
+++ platform/common/src/main/java/team/aura_dev/auraban/platform/common/AuraBanBase.java
@@ -3,18 +3,24 @@ package team.aura_dev.auraban.platform.common;
 import java.io.File;
 import java.util.Collection;
 import java.util.Collections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import team.aura_dev.auraban.api.AuraBanApi;
 import team.aura_dev.auraban.platform.common.dependency.DependencyDownloader;
 import team.aura_dev.auraban.platform.common.dependency.RuntimeDependency;
 
 public interface AuraBanBase extends AuraBanApi {
+  public static final Logger logger = LoggerFactory.getLogger(NAME);
+
   public File getConfigDir();
 
   public default void startPlugin() {
+    logger.info("Downloading early dependencies");
     DependencyDownloader.downloadAndInjectInClasspath(getEarlyDependencies(), getLibsDir());
 
     // TODO
 
+    logger.info("Downloading dependencies");
     DependencyDownloader.downloadAndInjectInClasspath(getDependencies(), getLibsDir());
   }
 
--- platform/common/src/main/java/team/aura_dev/auraban/platform/common/dependency/DependencyDownloader.java
+++ platform/common/src/main/java/team/aura_dev/auraban/platform/common/dependency/DependencyDownloader.java
@@ -3,13 +3,19 @@ package team.aura_dev.auraban.platform.common.dependency;
 import eu.mikroskeem.picomaven.PicoMaven;
 import java.io.File;
 import java.util.Collection;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 import lombok.experimental.UtilityClass;
+import team.aura_dev.auraban.platform.common.AuraBanBase;
 
 @UtilityClass
 public class DependencyDownloader {
   public static void downloadAndInjectInClasspath(
       Collection<RuntimeDependency> dependencies, File libPath) {
+    if (!(libPath.exists() || libPath.mkdirs())) {
+      throw new RuntimeException("Can't create the dirs");
+    }
+
     PicoMaven.Builder picoMavenBase =
         new PicoMaven.Builder()
             .withDownloadPath(libPath.toPath())
@@ -27,7 +33,17 @@ public class DependencyDownloader {
                     .collect(Collectors.toList()));
 
     try (PicoMaven picoMaven = picoMavenBase.build()) {
-      picoMaven.downloadAllArtifacts();
+      picoMaven
+          .downloadAllArtifacts()
+          .values()
+          .forEach(
+              future -> {
+                try {
+                  AuraBanBase.logger.info(future.get().toString());
+                } catch (InterruptedException | ExecutionException e) {
+                  e.printStackTrace();
+                }
+              });
 
       // TODO: Process the dependencies, add them to the classpath and relocate them
     }
