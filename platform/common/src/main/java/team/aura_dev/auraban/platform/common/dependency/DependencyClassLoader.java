1ef61fc7aafd5c8b295b55c5d354a57cf12b469c
BrainStone <yannick@tedworld.de>
2019-09-30 00:42:08 +0200

    Try loading from the own URLs first

--- platform/common/src/main/java/team/aura_dev/auraban/platform/common/dependency/DependencyClassLoader.java
+++ platform/common/src/main/java/team/aura_dev/auraban/platform/common/dependency/DependencyClassLoader.java
@@ -13,11 +13,41 @@ public class DependencyClassLoader extends URLClassLoader {
     // Steal the plugin {@link ClassLoader}'s URLs and parent.
     // Because else the plugin class would be loaded with the plugin {@link ClassLoader} and classes
     // below that wouldn't be loaded at all.
-    super(parent.getURLs(), parent.getParent());
+    super(getAuraBanURL(), parent);
   }
 
   @Override
   public void addURL(URL url) {
     super.addURL(url);
+  }
+
+  @Override
+  protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+    // has the class loaded already?
+    Class<?> loadedClass = findLoadedClass(name);
+
+    if (loadedClass == null) {
+      try {
+        // find the class from given jar urls
+        loadedClass = findClass(name);
+      } catch (ClassNotFoundException e) {
+        // class does not exist in the given urls.
+        // Let's try finding it in our parent classloader.
+        // this'll throw ClassNotFoundException in failure.
+        loadedClass = super.loadClass(name, resolve);
+      }
+    }
+
+    if (resolve) { // marked to resolve
+      resolveClass(loadedClass);
+    }
+
+    return loadedClass;
+  }
+
+  private static URL[] getAuraBanURL() {
+    return new URL[] {
+      DependencyClassLoader.class.getProtectionDomain().getCodeSource().getLocation()
+    };
   }
 }
