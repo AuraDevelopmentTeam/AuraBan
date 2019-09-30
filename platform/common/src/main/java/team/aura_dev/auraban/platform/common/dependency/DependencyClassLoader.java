06e49e93460bc12c5e5c91383d4d27723e9420b6
BrainStone <yannick@tedworld.de>
2019-09-30 23:33:15 +0200

    Fixed classloading and related issues
    Since this ClassLoader kinda refers to itself laoding from it's own path first is a bad idea. Unless you use already loaded classes (with refelctions).
    In short ClassLoader hacking BS

--- platform/common/src/main/java/team/aura_dev/auraban/platform/common/dependency/DependencyClassLoader.java
+++ platform/common/src/main/java/team/aura_dev/auraban/platform/common/dependency/DependencyClassLoader.java
@@ -1,19 +1,27 @@
 package team.aura_dev.auraban.platform.common.dependency;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 
 /** A custom {@link ClassLoader} implementation that allows adding URLs. */
 public class DependencyClassLoader extends URLClassLoader {
+  private static final Method findLoadedClassMethod = getFindLoadedClassMethod();
+
+  protected final ClassLoader parent;
+
   public DependencyClassLoader() {
-    this((URLClassLoader) DependencyClassLoader.class.getClassLoader());
+    this(DependencyClassLoader.class.getClassLoader());
   }
 
-  public DependencyClassLoader(URLClassLoader parent) {
+  public DependencyClassLoader(ClassLoader parent) {
     // Steal the plugin {@link ClassLoader}'s URLs and parent.
     // Because else the plugin class would be loaded with the plugin {@link ClassLoader} and classes
     // below that wouldn't be loaded at all.
     super(getAuraBanURL(), parent);
+
+    this.parent = parent;
   }
 
   @Override
@@ -25,6 +33,15 @@ public class DependencyClassLoader extends URLClassLoader {
   protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
     // has the class loaded already?
     Class<?> loadedClass = findLoadedClass(name);
+
+    // Reuse existing instances of own classes
+    if ((loadedClass == null) && (name.startsWith("@group@"))) {
+      try {
+        loadedClass = (Class<?>) findLoadedClassMethod.invoke(parent, name);
+      } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+        // Ignore
+      }
+    }
 
     if (loadedClass == null) {
       try {
@@ -49,5 +66,17 @@ public class DependencyClassLoader extends URLClassLoader {
     return new URL[] {
       DependencyClassLoader.class.getProtectionDomain().getCodeSource().getLocation()
     };
+  }
+
+  private static Method getFindLoadedClassMethod() {
+    try {
+      final Method method = ClassLoader.class.getDeclaredMethod("findLoadedClass", String.class);
+      method.setAccessible(true);
+
+      return method;
+    } catch (NoSuchMethodException | SecurityException e) {
+      // Can't continue
+      throw new IllegalStateException("Exception while trying to prepare the ClassLoader", e);
+    }
   }
 }
