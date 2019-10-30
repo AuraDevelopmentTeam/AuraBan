e12ccfac6ad4f578ef34342c12aa229cb6584113
BrainStone <yannick@tedworld.de>
2019-10-30 23:23:44 +0100

    Actually connecting to the MySQL server

--- platform/common/src/main/java/team/aura_dev/auraban/platform/common/storage/engine/MySQLStorageEngine.java
+++ platform/common/src/main/java/team/aura_dev/auraban/platform/common/storage/engine/MySQLStorageEngine.java
@@ -1,25 +1,37 @@
 package team.aura_dev.auraban.platform.common.storage.engine;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.RequiredArgsConstructor;
 import team.aura_dev.auraban.platform.common.storage.StorageEngine;
 
 @RequiredArgsConstructor
 public class MySQLStorageEngine implements StorageEngine {
+  private static final String URLFormat = "jdbc:mysql://%s:%d/%s";
+
   private final String host;
   private final int port;
   private final String database;
   private final String user;
   private final String password;
 
+  HikariDataSource dataSource;
+
   @Override
   public void initialize() {
-    // TODO Auto-generated method stub
+    HikariConfig config = new HikariConfig();
+    config.setJdbcUrl(String.format(URLFormat, host, port, database));
+    config.setUsername(user);
+    config.setPassword(password);
+    config.addDataSourceProperty("cachePrepStmts", true);
+    config.addDataSourceProperty("prepStmtCacheSize", 250);
+    config.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
 
+    dataSource = new HikariDataSource(config);
   }
 
   @Override
   public void close() throws Exception {
-    // TODO Auto-generated method stub
-
+    dataSource.close();
   }
 }
