305f2908e14be176319bb540b90f03732ff3ddd9
BrainStone <yannick@tedworld.de>
2019-12-24 00:31:10 +0100

    Nukkit needs to load late (like BungeeCord)

--- platform/nukkit/src/main/java/team/aura_dev/auraban/platform/nukkit/AuraBanNukkitBootstrap.java
+++ platform/nukkit/src/main/java/team/aura_dev/auraban/platform/nukkit/AuraBanNukkitBootstrap.java
@@ -4,9 +4,10 @@ import cn.nukkit.plugin.PluginBase;
 import team.aura_dev.auraban.platform.common.AuraBanBaseBootstrap;
 
 public class AuraBanNukkitBootstrap extends PluginBase {
-  private final AuraBanBaseBootstrap bootstrapPlugin;
+  private AuraBanBaseBootstrap bootstrapPlugin;
 
-  public AuraBanNukkitBootstrap() {
+  @Override
+  public void onLoad() {
     bootstrapPlugin = new AuraBanBaseBootstrap();
     bootstrapPlugin.checkAndLoadSLF4J(getDataFolder().toPath().resolve("libs"), "nukkit");
     bootstrapPlugin.initializePlugin(this, getServer(), getDataFolder().toPath());
